6990403c824fa38b8cba7577a65bc35bb37a0473
Wellington Marthas <well@dyntime.com>
2019-10-05 17:10:35 -0300

    Fix on id generation

--- src/nextid.c
+++ src/nextid.c
@@ -6,16 +6,21 @@
 #define OUR_EPOCH 1546304400000
 
 PG_MODULE_MAGIC;
- 
+
 PG_FUNCTION_INFO_V1(next_id);
 
+int64 current_timestamp(void);
+
 Datum next_id(PG_FUNCTION_ARGS) {
-    struct timeval t;
- 
-    int64 seq_id   = PG_GETARG_INT64(0);
+    int64 seq_id = PG_GETARG_INT64(0);
     int32 shard_id = PG_GETARG_INT32(1);
-    
+
+    PG_RETURN_INT64(((current_timestamp() << 23) | (shard_id << 10)) | (seq_id % 1024));
+}
+
+int64 current_timestamp(void) {
+    struct timeval t;
+
     gettimeofday(&t, NULL);
- 
-    PG_RETURN_INT64(((tp.tv_sec * 1000 + tp.tv_usec / 1000 - OUR_EPOCH) << 23) | (shard_id << 10) | (seq_id % 1024));
+    return t.tv_sec * 1000LL + t.tv_usec / 1000;
 }
